508c183c67bb0b3420d61792e8ec6bf3d4bd1e74
lakhankumar <lakhankumar997@gmail.com>
2021-07-27 15:51:48 +0530

    Added: doublet

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,27 +1,43 @@
-#!/bin/bash -x
+#!/bin/bash 
 
-declare -A singlet
+declare -A doublet
 declare -A percent
-read -p "Enter how many times you want to flip coin: " num
-head=0
-tail=0
+num=21
+TT=0
+TH=0
+HT=0
+HH=0
 for((i=1; i<=$num; i++))
 do
-toss=$(($RANDOM%2))
-if [ $toss -eq 1 ]
+toss1=$(($RANDOM%2))
+toss2=$(($RANDOM%2))
+if [ $toss1 -eq 0 ] && [ $toss2 -eq 0 ] 
 then
-	echo "Its head"
-	head=$(($head+1))
+	echo "Its TT"
+	TT=$(($TT+1))
+elif  [ $toss1 -eq 0 ] && [ $toss2 -eq 1 ]
+then
+   echo "Its TH"
+   TH=$(($TH+1))
+elif [ $toss1 -eq 1 ] && [ $toss2 -eq 0 ]
+then
+   echo "Its HT"
+   HT=$(($HT+1))
+elif  [ $toss1 -eq 1 ] && [ $toss2 -eq 1 ]
+then
+   echo "Its HH"
+   HH=$(($HH+1))
 else
-	echo "Its tail"
-	tail=$(($tail+1))
-fi
-done
-singlet[H]=$head
-singlet[T]=$tail
-echo "no.of head= $head"
-echo "no.of tail= $tail"
-echo "${singlet[*]}"
-percent[H]=$(($head*100/21))
-percent[T]=$(($tail*100/21))
-echo "${percent[*]}"
+	echo "invalid"
+fi 
+done 
+doublet[TT]=$TT 
+doublet[TH]=$TH
+doublet[HT]=$HT
+doublet[HH]=$HH
+echo "doublet: ${doublet[*]}"
+percent[TT]=$(($TT*100/21))
+percent[TH]=$(($TH*100/21))
+percent[HT]=$(($HT*100/21))
+percent[HH]=$(($HH*100/21)) 
+echo "percent: ${percent[*]}"
